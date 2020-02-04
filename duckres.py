3d2309700547d7618ccc1646e0808699a606e2ce
Gr33nHack3r <59998428+Gr33nHack3r@users.noreply.github.com>
2020-02-04 19:07:49 +0100

    Update and rename imgconv.py to duckres.py

---
+++ duckres.py
@@ -0,0 +1,16 @@
+def prop(x_image,y_image,x_ratio,y_ratio):
+    divimg = (x_image / y_image)
+    divrat = (x_ratio / y_ratio)
+    
+    n = len(str(x_image)) - 2
+           
+    if divimg <= divrat:
+        print("entrato1")
+        while round((x_image / y_image), n) != round((x_ratio / y_ratio), n):
+            y_image -= 1
+        print("x = " + str(x_image)), print("y = " + str(y_image))
+        
+    elif divimg > divrat:
+        while round((x_image / y_image), n) != round((x_ratio / y_ratio), n):   
+            x_image -= 1
+        print("x = " + str(x_image)), print("y = " + str(y_image))
